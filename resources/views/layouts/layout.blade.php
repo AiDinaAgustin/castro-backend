773965bfdd94dcf3f590548615024ee6d57a05b5
AiDinaAgustin <aidinagustin2@gmail.com>
2023-11-29 16:21:50 +0700

    menampilkan podcast

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -4,14 +4,10 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://cdn.tailwindcss.com"></script>
-  {{-- favicon --}}
-  <link rel="shortcut icon" href="assets/img/LOGO CASTRO (2) 2.png" type="image/x-icon">
   <!-- Link Swiper's CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
-  {{-- Link CSS --}}
-  <link rel="stylesheet" href="css/style.css">
   <style>
-    /* main::-webkit-scrollbar {
+    main::-webkit-scrollbar {
       width: 12px;
     }
   
@@ -41,7 +37,6 @@
       opacity: 1;
       bottom: 10px;
     }
-
     .show {
       display: block !important;
       width: 100%;
@@ -51,10 +46,14 @@
       transition: width 0.4s ease-in;
     }
 
-    .active {
-      color: white;
-      border-bottom: 2px solid #F9D80F;
-    } */
+    .cover {
+      background-image: url(assets/img/horor2.jpg);
+      background-repeat: no-repeat;
+      background-size: cover;
+      width: 100%;
+      height: 500px;
+      filter: blur(10px);
+    }
   </style>  
   <title>Castro Studio</title>
 </head>
@@ -64,58 +63,17 @@
       <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 512 512"><path d="M470.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 256 265.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160zm-352 160l160-160c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L210.7 256 73.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0z"/></svg>
     </div>
     <div class="flex items-start gap-4">
-      {{-- Navbar --}}
       @include('partials.navbar')
-      <div class="overflow-hidden flex-1 bg-zinc-900 rounded-lg mx-auto p-4">
-        {{-- header --}}
-       
-        {{-- main --}}
+      <div class="flex-1 bg-zinc-900 rounded-lg mx-auto py-2 px-4">
+        
+        {{-- Main content --}}
         @yield('content')
       </div>
     </div>
   </div>
 </body>
-<script>
-  // Time at now 
-  const timeDays = () => {
-    let time = new Date()
-    let hours = time.getHours()
-    let greeting
-    if (hours >= 0 && hours < 12) {
-      greeting = 'Good Morning';
-    } else if (hours >= 12 && hours < 18) {
-      greeting = 'Good Afternoon';
-    } else {
-      greeting = 'Good Evening';
-    }
-    return greeting
-  }
-
-  const updateTimeDisplay = () => {
-      const timeDisplay = document.getElementById('timeDisplay');
-      timeDisplay.textContent = timeDays();
-    }
-
-    setInterval(updateTimeDisplay, 1000);
-
-    updateTimeDisplay();
-</script>
 <!-- Swiper JS -->
 <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-
-<!-- Initialize Swiper -->
-<script>
-  const mySwiper = new Swiper('.mySwiper', {
-    // Default parameter
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-    },
-    centeredSlides: true,
-  });
-</script>
 <!-- swiper card -->
 <script>
   const swiper = new Swiper('.swiper', {
@@ -153,11 +111,56 @@
   });
 </script>
 <script>
+  // button share 
+  const btnShare = document.querySelector('.btn-share')
+  btnShare.addEventListener('click', () => {
+    const currentURL = window.location.href;
+    const dummy = document.createElement('textarea');
+    dummy.value = currentURL;
+    document.body.appendChild(dummy);
+    dummy.select();
+    document.execCommand('copy');
+    document.body.removeChild(dummy);
+
+    // Tampilkan alert
+    alert('URL berhasil disalin!');
+  })
+</script>
+<script>
   // show modal menu 
   const showButton = document.querySelector('.show-btn');
   const modalMenu = document.querySelector('nav');
   showButton.addEventListener(('click'), ()=> {
     modalMenu.classList.toggle('show');
+  })
+</script>
+<script>
+  const playbtns = document.querySelectorAll('.play_btn')
+  const pausebtns = document.querySelectorAll('.pause_btn')
+  pausebtns.forEach((pausebtn) => {
+    pausebtn.style.display = 'none'
+  })
+
+  playbtns.forEach((playbtn) => {
+    playbtn.addEventListener('click', () => {
+      pausebtns.forEach((pausebtn) => {
+        pausebtn.style.display = 'block'
+      })
+      playbtns.forEach((playbtn) => {
+        playbtn.style.display = 'none'
+      })
+    })
+  })
+
+  pausebtns.forEach((pausebtn, index) => {
+    pausebtn.addEventListener('click', () => {
+      playbtns.forEach((playbtn) => {
+        playbtn.style.display = 'block'
+      })
+      pausebtns.forEach((pausebtn) => {
+        pausebtn.style.display = 'none'
+      })
+    })
   })
 </script>
 
