5ab55b4b651bafc35c8561ce765adc412cb71f31
AiDinaAgustin <aidinagustin2@gmail.com>
2023-11-30 17:07:14 +0700

    PodcastFactory

---
+++ database/factories/PodcastFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Podcast>
+ */
+class PodcastFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(mt_rand(3, 10)),
+            'slug' => $this->faker->slug(),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Podcast;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +21,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::factory(5)->create();
     }
 }
