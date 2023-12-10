21db1d785192860d8590dbe2416df0c4e56654f4
AiDinaAgustin <aidinagustin2@gmail.com>
2023-12-10 20:34:23 +0700

    Add search bar to cari.blade.php

--- resources/views/cari.blade.php
+++ resources/views/cari.blade.php
@@ -1,6 +1,22 @@
 @extends('layouts.layout')
 
 @section('content')
+    <header class="sticky top-0 bg-zinc-900 z-50 flex items-center justify-between mb-4 py-2">
+        <div class="button-nav flex items-center">
+        <div class="btn-undo mr-4">
+            <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 512 512" fill="#F9D80F"><path d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM271 135c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-87 87 87 87c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L167 273c-9.4-9.4-9.4-24.6 0-33.9L271 135z"/></svg>
+        </div>
+        <div class="btn-redo mr-4">
+            <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 512 512" fill="#F9D80F"><path d="M0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM241 377c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l87-87-87-87c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0L345 239c9.4 9.4 9.4 24.6 0 33.9L241 377z"/></svg>
+        </div>
+        <div class="relative search-btn w-72 h-8 bg-black rounded-full overflow-hidden px-4">
+            <div class="absolute translate-y-2/4 top-0 left-2">
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" fill="white"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
+            </div>
+            <input class="w-full h-full ml-5 text-white bg-transparent outline-none" type="search" placeholder="What do you want to listen?">
+        </div>
+        </div>
+    </header>
     <main class="w-full min-h-screen overflow-x-hidden">
           <!-- Main content -->
           <!-- Browse All -->
